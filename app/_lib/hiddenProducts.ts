87ebcfe81a37a6440235d23daad40aa3b6c1ea0a
rayyy9292 <arinamontoss@gmail.com>
2026-09-03 13:15:58 +0500

    Hide duplicate Mokko table product

--- app/_lib/hiddenProducts.ts
+++ app/_lib/hiddenProducts.ts
@@ -1,6 +1,7 @@
 export const hiddenProductSlugs = new Set([
   "main-shkaf-liana",
   "shkaf-liana",
+  "telegram-stol-mokko-so-stulyami-mokko",
 ]);
 
 export function isHiddenProductSlug(slug: string) {
