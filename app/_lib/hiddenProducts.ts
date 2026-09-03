a41529b3190026bcdfbc6ae972a58c2657c51020
rayyy9292 <arinamontoss@gmail.com>
2026-09-03 13:34:49 +0500

    Hide duplicate Vivaldi bedroom product

--- app/_lib/hiddenProducts.ts
+++ app/_lib/hiddenProducts.ts
@@ -2,6 +2,7 @@ export const hiddenProductSlugs = new Set([
   "main-shkaf-liana",
   "shkaf-liana",
   "spalnya-dia-6dv",
+  "spalnya-vivaldi-6ti-2",
   "stol-graciya",
   "stol-graciya-oval",
   "telegram-stol-afina-so-stulyami-suren",
