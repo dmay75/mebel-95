52c130fd460bb03282e345ba6f5d1547473974fa
rayyy9292 <arinamontoss@gmail.com>
2026-09-03 13:32:34 +0500

    Hide duplicate Dia bedroom product

--- app/_lib/hiddenProducts.ts
+++ app/_lib/hiddenProducts.ts
@@ -1,6 +1,7 @@
 export const hiddenProductSlugs = new Set([
   "main-shkaf-liana",
   "shkaf-liana",
+  "spalnya-dia-6dv",
   "stol-graciya",
   "stol-graciya-oval",
   "telegram-stol-afina-so-stulyami-suren",
